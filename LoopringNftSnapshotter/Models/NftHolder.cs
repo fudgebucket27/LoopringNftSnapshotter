dfef70c8c2cf1a32cc8748b2106b2b34f9cd423d
Fudgey <fudgebucket27@gmail.com>
2022-08-04 18:31:32 +1000

    add stopwatch and example text file

---
+++ LoopringNftSnapshotter/Models/NftHolder.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoopringNftSnapshotter.Models
+{
+    public class NftHolder
+    {
+        public string? address { get; set; }
+        public string? nftId { get; set; }
+    }
+}
--- LoopringNftSnapshotter/Program.cs
+++ LoopringNftSnapshotter/Program.cs
@@ -1,9 +1,66 @@
-﻿using LoopringNftSnapshotter.Models;
+﻿using CsvHelper;
+using LoopringNftSnapshotter.Models;
 using LoopringNftSnapshotter.Services;
+using System.Diagnostics;
+using System.Globalization;
 
+//Initialize objects
 LoopringGraphQLService loopringGraphQLService = new LoopringGraphQLService("https://api.thegraph.com/subgraphs/name/juanmardefago/loopring36");
-List<AccountNFTSlot> nftHolders = await loopringGraphQLService.GetNftHolders("0x39dc73b6067f33f1225695059512fa889b8cef6b-0-0x1d006a27bd82e10f9194d30158d91201e9930420-0x13a4661f9339947e56a2c16e704ad6b9c77a5a20cd212272c26959fdad0ccd2e-5");
-foreach(var nftHolder in nftHolders)
+List<string> metaboyNftIds = new List<string>();
+List<NftHolder> metaboyHolders = new List<NftHolder>();
+List<NftHolder> errors = new List<NftHolder>();
+
+
+//Load nfts from text file
+using(StreamReader sr = new StreamReader("metaboys.txt"))
 {
-    Console.WriteLine(nftHolder.account!.address);
+    string metaboyId;
+    while ((metaboyId = sr.ReadLine()!) != null)
+    {
+        metaboyNftIds.Add(metaboyId);
+    }
+}
+
+Console.WriteLine("Working...");
+//Loop through nft ids and call graph ql service to get holders
+
+Stopwatch stopWatch = new Stopwatch();
+stopWatch.Start();
+foreach(string metaboyNftId in metaboyNftIds)
+{
+    List<AccountNFTSlot> accountNftSlots = await loopringGraphQLService.GetNftHolders($"0x39dc73b6067f33f1225695059512fa889b8cef6b-0-0x1d006a27bd82e10f9194d30158d91201e9930420-{metaboyNftId}-5");
+    if(accountNftSlots.Count == 0) //Issue with the graph
+    {
+        errors.Add(new NftHolder() { address = "Could not find!", nftId = metaboyNftId });
+    }
+    else
+    {
+        foreach (var nftHolder in accountNftSlots)
+        {
+            metaboyHolders.Add(new NftHolder() { address = nftHolder.account!.address, nftId = metaboyNftId });
+        }
+    }
+    break;
+}
+stopWatch.Stop();
+Console.WriteLine($"Gathered holders in {stopWatch.Elapsed.ToString("hh\\:mm\\:ss\\.ff")}");
+
+
+
+//Create CSV report
+string dateTime = DateTime.Now.ToString("yyyy-mm-dd hh-mm-ss");
+string holderCsvName = $"Holders-{dateTime}.csv";
+string errorCsvName = $"Errors-{dateTime}.csv";
+using (var writer = new StreamWriter(holderCsvName))
+using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+{
+    csv.WriteRecords(metaboyHolders);
+    Console.WriteLine($"Generated Holder Report and can be found in the following location: {AppDomain.CurrentDomain.BaseDirectory + holderCsvName}");
+}
+
+using (var writer = new StreamWriter(errorCsvName))
+using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+{
+    csv.WriteRecords(errors);
+    Console.WriteLine($"Generated Holder Report and can be found in the following location: {AppDomain.CurrentDomain.BaseDirectory + errorCsvName}");
 }
